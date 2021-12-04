6f21c748266c795a3083931c64ac8aece302ad30
Vi Roesler <vroesler@leafpuddle.com>
2021-12-04 01:08:40 -0600

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string input = System.IO.File.ReadAllText("./input.txt");
+        string[] inputs = input.Split(
+            new string[] { Environment.NewLine },
+            StringSplitOptions.None
+        );
+
+        int result = 0;
+
+        for(int i = 0; i < inputs.Length; i++)
+        {
+            if( i == 0 ) continue;
+
+            int.TryParse(inputs[i], out int a);
+            int.TryParse(inputs[i - 1], out int b);
+
+            if( a > b ) result++;
+        }
+
+        Console.WriteLine("Result: " + result);
+        Console.ReadKey();
+    }
+}
